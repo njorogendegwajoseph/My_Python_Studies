48c4223bb0aff9350883a33e2375a49d1a8002cb
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-09-24 11:15:57 +0300

    Added some extra files on strings

---
+++ Strings/task1.py
@@ -0,0 +1,11 @@
+x = "Hello, Sir!"
+print(x)
+#Just prints Hello, sir!
+
+y = "My name is Arthur Vandawick!\t"
+print(len(y))
+#gives the character count of the string.
+
+
+print(8 * y)
+#This prints the name eight times.
